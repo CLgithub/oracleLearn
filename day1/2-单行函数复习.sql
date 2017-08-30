3f75e75384ad063f96813f7bca2286c44495026c
UltraPowerAW <leichen070@gmail.com>
2017-08-30 21:10:01 +0800

    单行函数复习2-日期函数

--- day1/2-单行函数复习.sql
+++ day1/2-单行函数复习.sql
@@ -36,11 +36,35 @@ select sysdate-1 昨天,sysdate 今天, sysdate+1 明天 from dual;
 select e.ename,e.hiredate,trunc((sysdate-e.hiredate),0) 天,
 trunc((sysdate-e.hiredate)/7,0) 周,(sysdate-e.hiredate)/30 月,(sysdate-e.hiredate)/365
  from emp e
-
+------日期函数
+------months_between(a,b) a日期与b日期相差多少个月
+--------使用日期函数months_between精确计算月份  天可以用- 年可以用月*12 所有需要一个计算月的方法
+select e.ename,e.hiredate,sysdate-e.hiredate 天,
+months_between(sysdate,e.hiredate) 月,months_between(sysdate,e.hiredate)/12 年
+from emp e;
+--------生命
+select (sysdate-to_date('19900222','yyyymmdd')) 天,months_between(sysdate,to_date('19900222','yyyymmdd'))月,
+months_between(sysdate,to_date('19900222','yyyymmdd'))/12 年
+from dual;
+------add_months(a,b) 在a日期上加上b个数月后的日期
+--------今天一个月以后的日期
+select add_months(sysdate,1) from dual;
+select add_months(sysdate,-3) from dual;
+------last_day(a)  a日期所在月份的最后一天的日期
+select last_day(sysdate) from dual;
+------next_day(a,星期b)  从a日期开始，算出下一个'星期b'的日期
+select next_day(sysdate,'星期日') from dual;
+--------应用，每周一自动备份数据
+------round(a,b) 日期四舍五入,a日期四舍五入为b,b 'day' 'month' 'year'
+select round(sysdate,'day') from dual;
+select round(sysdate,'month') from dual;    --过来当月一半，就算下一个月
+select round(sysdate,'year') from dual;     --过了当年一半，就算下一年
+------trunc(a,b) 日期截取，a日期截取为b，b 'day' 'month' 'year'
+select trunc(sysdate,'day') from dual;
+select trunc(sysdate,'month') from dual;
+select trunc(sysdate,'year') from dual;
 ----4.转换函数
 
 ----5.通用函数
 
 ----6.条件表达式
-
-
